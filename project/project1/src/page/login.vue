2a53abc4a7e89ff23f4ca1fd84c3e6a6ab410a8b
fuhuiru <2782028534@qq.com>
2019-07-10 11:40:32 +0800

    用户登录

--- project/project1/src/page/login.vue
+++ project/project1/src/page/login.vue
@@ -1,5 +1,25 @@
 <template>
     <div>
-        <h1>login</h1>
+        <div class="container">
+            <h2>用户登录</h2>
+            <form>
+                <div class="form-group">
+                    <label>用户名</label>
+                    <input type="text" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label>密 码</label>
+                    <input type="text" class="form-control">
+                </div><div class="form-group">
+                    <input type="submit" class="btn btn-block btn-primary" value="登录">
+                </div>
+            </form>
+        </div>
     </div>
 </template>
+<style scoped>
+    h2{
+        text-align: center;
+        margin-top: 60px;
+    }
+</style>
