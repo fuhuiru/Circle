0982cefa6fad16950af552ca3e5432adce854615
fuhuiru <2782028534@qq.com>
2019-07-10 11:30:34 +0800

    用户注册

--- project/project1/src/page/signup.vue
+++ project/project1/src/page/signup.vue
@@ -1,5 +1,25 @@
 <template>
     <div>
-        <h1>signup</h1>
+        <div class="container">
+            <h2>用户注册</h2>
+            <form>
+                <div class="form-group">
+                    <label>用户名</label>
+                    <input type="text" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label>密 码</label>
+                    <input type="text" class="form-control">
+                </div><div class="form-group">
+                    <input type="submit" class="btn btn-block btn-primary">
+                </div>
+            </form>
+        </div>
     </div>
 </template>
+<style scoped>
+    h2{
+        text-align: center;
+        margin-top: 60px;
+    }
+</style>
