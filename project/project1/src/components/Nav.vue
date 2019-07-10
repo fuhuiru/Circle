e1d48358b3b8f15810e608341bb685be546d850e
fuhuiru <2782028534@qq.com>
2019-07-10 20:21:41 +0800

    完善导航组件

--- project/project1/src/components/Nav.vue
+++ project/project1/src/components/Nav.vue
@@ -14,48 +14,70 @@
           </div>
         </div>
         <div class="col user">
-          <span>
+          <span v-if="session.loggedIn().sessionid">
+            <router-link to="/login">{{session.loggedIn().user.username}}</router-link>
+            <a href="#" @click="logout()">退出</a>
+          </span>
+          <span v-else>
             <router-link to="/login">登录</router-link>
             <router-link to="/signup">注册</router-link>
-          </span>
-           <span>
-            <router-link to="/login">用户名</router-link>
-            <router-link to="/signup">退出</router-link>
           </span>
         </div>
       </div>
     </div>
   </div>
 </template>
+<script>
+import session from "../lib/session";
+export default {
+  data() {
+    return {
+      session,
+      // userdata = [];
+    };
+  },
+  methods: {
+    logout(){
+      console.log("dvdasv");
+      this.session.logout();
+  }
+  },
+  mounted() {
+   console.log( this.session.loggedIn.user);
+  }
+}
+</script>
 <style scoped>
 .text-right {
   text-align: right;
 }
-.nav{
+.nav {
   padding-top: 10px;
   padding-bottom: 10px;
 }
 .nav .search input,
-.nav .search button
-{
+.nav .search button {
   vertical-align: top;
   outline: none;
   height: 46px;
 }
-.nav .search input{
+.nav .search button {
+  width: 68px;
+}
+.nav .search input {
   padding-left: 10px;
   padding-right: 10px;
 }
-.user{
+.user {
   margin-top: 10px;
 }
-.user > *{
-  font-size: 1.1em;
+.user > * {
+  font-size: 1.04em;
   vertical-align: middle;
 }
 
-.user span > *{
+.user span > * {
   color: #222;
-  margin-left: 8px;
+  margin-left: 2px;
 }
 </style>
